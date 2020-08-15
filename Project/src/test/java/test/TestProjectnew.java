f51edf456b617e750bde83a6d9189f7c9b38529c
ravib <ravib@LAPTOP-KFNOH638>
2020-08-15 20:06:26 +0100

    second commit and added new class

---
+++ Project/src/test/java/test/TestProjectnew.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestProjectnew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Animal an=new Animal();
+		an.drink();
+
+	}
+
+}
